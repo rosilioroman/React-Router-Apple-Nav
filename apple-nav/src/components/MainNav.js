104f03a69ec9a6ddb63d7fdd3d73a79a47007866
rosilioroman <rosilioroman@gmail.com>
2019-01-29 17:46:02 -0700

    Convert MainNav to styled components. Add 'exact' prop to home Route

--- apple-nav/src/components/MainNav.js
+++ apple-nav/src/components/MainNav.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import styled from 'styled-components';
 
 //components
 import MainLinksContainer from './MainLinksContainer';
@@ -8,14 +9,45 @@ import Apple from '../images/apple-icon.png';
 import Search from '../images/search-icon.png';
 import Bag from '../images/bag-icon.png';
 
+//Styled Components
+const MainNavWrapper = styled.ul`
+    list-style: none;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 2.5vw;
+    margin: 0;
+    background-color: #343434;
+    color: #fff;
+`;
+
+const MainNavContent = styled.div`
+    width: 60%;
+    display: flex;
+    justify-content: space-around;
+`;
+
+const SearchIcon = styled.img`
+    margin-top: 4px;
+`;
+
+const IconLink = styled.li`
+    &:hover {
+        cursor: pointer;
+    }
+`;
+
 const MainNav = props => {
     return (
-        <ul>
-            <li><img src={Apple} alt="apple icon"/></li>
-            <MainLinksContainer linksList={props.linksList}/>
-            <li><img src={Search} alt="search icon"/></li>
-            <li><img src={Bag} alt="bag icon"/></li>
-        </ul>
+        <MainNavWrapper>
+            <MainNavContent>
+                <IconLink><img src={Apple} alt="apple icon"/></IconLink>
+                <MainLinksContainer linksList={props.linksList}/>
+                <IconLink><SearchIcon src={Search} alt="search icon"/></IconLink>
+                <IconLink><img src={Bag} alt="bag icon"/></IconLink>
+            </MainNavContent>
+        </MainNavWrapper>
     );
 }
 
